15086b6942137dc4aef6751a510a77a1a3fc90da
Yapa Bigford <110362211+ybigford@users.noreply.github.com>
2024-01-07 21:22:49 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let firstName = "Yapa";
+let lastName = "Bigford";
+let yearsOfStudy = "2 years";
+let goal = "graduate and pursue my higher degree. I would like to become a professional software developer.";
+
+
+document.write(`My name is ${firstName} ${lastName} <br> I have been at SCC for  ${yearsOfStudy} and plan to <br> ${goal} after completing my courses. <br> `);
+        
+document.write('My name is ' +  firstName + ' ' + lastName +  'I have been at SCC for ' + yearsOfStudy + ' and plan to ' + goal + ' after completing my courses');
+
