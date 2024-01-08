b69135de918eb0bdb631e46540014a75f62b944a
Yapa Bigford <110362211+ybigford@users.noreply.github.com>
2024-01-07 22:22:43 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let firstName = "Yapa";
 let lastName = "Bigford";
 let yearsOfStudy = "2 years";
 let goal = "graduate and pursue my higher degree. I would like to become a professional software developer.";
+let dog1 = "Bucky";
+let dog2 = "Wagner";
 
 
 document.write(`My name is ${firstName} ${lastName} <br> I have been at SCC for  ${yearsOfStudy} and plan to <br> ${goal} after completing my courses. <br> `);
